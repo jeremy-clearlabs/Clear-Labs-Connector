e88a061b0491204b5637ee7583643be85347e766
jermspeaks <jermspeaks@gmail.com>
2018-11-27 15:13:55 -0800

    Add get data and schema

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ function resetAuth() {
  * Check if the provided username and password are valid through a
  * call to your service. You would have to have a `checkForValidCreds`
  * function defined for this to work.
- * @param {String} email 
- * @param {String} password 
+ * @param {String} email
+ * @param {String} password
  * @returns {Boolean|String} Returns false or access token
  */
 function checkForValidCreds(email, password) {
@@ -32,18 +32,20 @@ function checkForValidCreds(email, password) {
 
   var body = JSON.stringify({ email: email, password: password });
   // POST /login
-  var response = UrlFetchApp.fetch('https://api-dev.clearlabs.com/login', {
-    method: 'post',
-    body: body
+  var response = UrlFetchApp.fetch('http://api-dev.clearlabs.com/login', {
+    method: 'POST',
+    payload: body,
+    muteHttpExceptions: true
   });
 
-  Logger.log(response.getContentText());
+  console.log('checkForValidCreds', response.getContentText());
 
   // Check if access token is returned
   if (!response) {
     return false;
   }
-  var res = response.json();
+  var res = JSON.parse(response);
+  Logger.log(res);
 
   if (res.access_token) {
     return res.access_token;
@@ -58,20 +60,26 @@ function validateCredentials(email, password) {
     return false;
   }
 
+  console.log('creating login request body');
   var body = JSON.stringify({ email: email, password: password });
-  // POST /login
-  var response = UrlFetchApp.fetch('https://api-dev.clearlabs.com/login', {
+  var options = {
     method: 'post',
-    body: body
-  });
+    payload: body
+  };
+  console.log(options);
 
-  Logger.log(response.getContentText());
+  var response = UrlFetchApp.fetch(
+    'http://api-dev.clearlabs.com/login',
+    options
+  );
+
+  console.log(response.getContentText());
 
   // Check if access token is returned
   if (!response) {
     return false;
   }
-  var res = response.json();
+  var res = JSON.parse(response);
 
   return !!res.access_token;
 }
@@ -99,6 +107,7 @@ function isAuthValid() {
  * @returns {Object} Error object with error or no error
  */
 function setCredentials(request) {
+  Logger.log('setCredentials', request);
   var creds = request.userPass;
   var username = creds.username;
   var password = creds.password;
@@ -128,5 +137,122 @@ function setCredentials(request) {
  * an admin user of the connector.
  */
 function isAdminUser() {
+  // var userProperties = PropertiesService.getUserProperties();
+  // var accessToken = userProperties.getProperty('dscc.accessToken');
+  // return !!accessToken;
   return true;
+}
+
+function getCompanyCount() {
+  console.log('getCompanyCount');
+  var userProperties = PropertiesService.getUserProperties();
+  var accessToken = userProperties.getProperty('dscc.accessToken');
+  if (!accessToken) {
+    console.log('User not logged in');
+    return {
+      errorCode: 'User not logged in'
+    };
+  }
+
+  var options = {
+    method: 'post',
+    headers: {
+      authorization: 'Bearer ' + accessToken,
+      'content-type': 'application/json'
+    },
+    payload:
+      '{"query":"query getAllCompanies { allCompanies { name, id, labs { id, name } } }","operationName":"getAllCompanies"}'
+  };
+  Logger.log(options);
+
+  var response = UrlFetchApp.fetch(
+    'http://api-dev.clearlabs.com/graphql',
+    options
+  );
+
+  console.log(response.getContentText());
+
+  var res = JSON.parse(response);
+  try {
+    return res.data.allCompanies.length;
+  } catch (error) {
+    return new Error('Data malformed');
+  }
+}
+
+function getConfig(request) {
+  var config = {
+    configParams: [
+      {
+        type: 'INFO',
+        name: 'Instructions',
+        text: 'Enter the env url e.g. dev, qa, staging, prod.'
+      },
+      {
+        type: 'TEXTINPUT',
+        name: 'env',
+        displayName: 'Env URL',
+        helpText: 'e.g. dev, qa, staging, prod',
+        placeholder: 'env url'
+      }
+    ],
+    dateRangeRequired: false
+  };
+  return config;
+}
+
+var dataSchema = [
+  {
+    name: 'companyCount',
+    dataType: 'NUMBER',
+    semantics: {
+      conceptType: 'METRIC',
+      semanticType: 'NUMBER',
+      isReaggregatable: true
+    },
+    defaultAggregationType: 'SUM'
+  }
+];
+
+function getSchema(request) {
+  return { schema: dataSchema };
+}
+
+function getData(request) {
+  console.log('getData', request);
+  var dataFetcher = [];
+  // Create schema for requested fields
+  var requestedSchema = request.fields.map(function(field) {
+    for (var i = 0; i < dataSchema.length; i++) {
+      if (dataSchema[i].name === field.name) {
+        // Setup data fetcher
+        dataFetcher.push(getCompanyCount);
+        return dataSchema[i];
+      }
+    }
+  });
+
+  console.log('schema', requestedSchema);
+
+  // Fetch and parse data from API
+  var responseData = [];
+  dataFetcher.forEach(function(fetch) {
+    var response = fetch();
+    responseData.push(response);
+  });
+
+  console.log('responseData', responseData);
+
+  var requestedData = responseData.map(function(companyCount) {
+    var values = [];
+    // only have company count
+    values.push(companyCount);
+
+    return { values: values };
+  });
+
+  return {
+    schema: requestedSchema,
+    rows: requestedData
+  };
 }
